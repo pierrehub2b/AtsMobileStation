f93c3bba4aae1d08bc1cdccf9242c85b1140b175
pierrehub2b <pierrehuber@yahoo.fr>
2019-08-12 22:08:15 +0200

    update router

--- src/assets/drivers/router.php
+++ src/assets/drivers/router.php
@@ -1,14 +1,19 @@
 <?php
 
-$url = 'http://localhost:8080'.$_SERVER['REQUEST_URI'];
-$ch = curl_init($url);
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+	$url = 'http://localhost:8080'.$_SERVER['REQUEST_URI'];
+	$ch = curl_init($url);
+	
+	curl_setopt($ch, CURLOPT_POST, 1);
+	curl_setopt($ch, CURLOPT_POSTFIELDS, "");
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	
+	$response = curl_exec($ch);
+	curl_close($ch);
+	
+	echo $response;
+}else{
+	echo "AtsMobileStation started ...";
+}
 
-curl_setopt($ch, CURLOPT_POST, 1);
-curl_setopt($ch, CURLOPT_POSTFIELDS, "");
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-
-$response = curl_exec($ch);
-curl_close($ch);
-
-echo $response;
 ?>
