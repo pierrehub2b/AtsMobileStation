ebfb77527d25f2b95f170445a3db34a5a5956f4e
pierrehub2b <pierrehuber@yahoo.fr>
2019-08-12 20:50:58 +0200

    get parameter

--- src/assets/drivers/router.php
+++ src/assets/drivers/router.php
@@ -1,5 +1,8 @@
 <?php
-$url = 'http://localhost/info';
+
+$parameter = $_SERVER['QUERY_STRING'];
+
+$url = 'http://localhost:8080/info';
 $ch = curl_init($url);
 
 curl_setopt($ch, CURLOPT_POST, 1);
@@ -9,5 +12,5 @@ curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 $response = curl_exec($ch);
 curl_close($ch);
 
-echo $response;
+echo $parameter;
 ?>
