72dee36d965777ac1807c57856b34be2dbd8d86c
pierrehub2b <pierrehuber@yahoo.fr>
2019-08-13 09:28:24 +0200

    fix post data

--- src/assets/drivers/router.php
+++ src/assets/drivers/router.php
@@ -1,21 +1,22 @@
 <?php
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+	$data = file_get_contents("php://input");
 	$url = 'http://localhost:8080'.$_SERVER['REQUEST_URI'];
 	$ch = curl_init($url);
 	
 	curl_setopt($ch, CURLOPT_POST, 1);
-	curl_setopt($ch, CURLOPT_POSTFIELDS, $HTTP_RAW_POST_DATA);
+	curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 	
 	$response = curl_exec($ch);
 	curl_close($ch);
 	
-	$verif = "";
+	/*$verif = "";
 	foreach($_POST as $value){
     	$verif .= $value;
-	}
-	echo "{'data':'".$HTTP_RAW_POST_DATA."'}";
+	}*/
+	echo "{'data':'".$data."'}";
 }else{
 	$hostPort = explode(":", $_SERVER['HTTP_HOST']);
 	echo "ATS MobileStation started on port : " + $hostPort[1];
