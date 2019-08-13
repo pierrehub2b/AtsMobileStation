a001a7d7c891d92c3b11ad42f9b8af3e73819d91
pierrehub2b <pierrehuber@yahoo.fr>
2019-08-13 09:25:07 +0200

    fix

--- src/assets/drivers/router.php
+++ src/assets/drivers/router.php
@@ -5,7 +5,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 	$ch = curl_init($url);
 	
 	curl_setopt($ch, CURLOPT_POST, 1);
-	curl_setopt($ch, CURLOPT_POSTFIELDS, $_POST);
+	curl_setopt($ch, CURLOPT_POSTFIELDS, $HTTP_RAW_POST_DATA);
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 	
 	$response = curl_exec($ch);
@@ -15,7 +15,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 	foreach($_POST as $value){
     	$verif .= $value;
 	}
-	echo "{'data':$verif}";
+	echo "{'data':'".$HTTP_RAW_POST_DATA."'}";
 }else{
 	$hostPort = explode(":", $_SERVER['HTTP_HOST']);
 	echo "ATS MobileStation started on port : " + $hostPort[1];
