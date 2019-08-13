ed7fc1a9411eddb4033bc9c8e1ffafd9475cfcd1
pierre huber <pierrehuber@yahoo.fr>
2019-08-13 09:32:51 +0200

    set final post data

--- src/assets/drivers/router.php
+++ src/assets/drivers/router.php
@@ -11,12 +11,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 	
 	$response = curl_exec($ch);
 	curl_close($ch);
-	
-	/*$verif = "";
-	foreach($_POST as $value){
-    	$verif .= $value;
-	}*/
-	echo "{'data':'".$data."'}";
+
+	echo $response;
 }else{
 	$hostPort = explode(":", $_SERVER['HTTP_HOST']);
 	echo "ATS MobileStation started on port : " + $hostPort[1];
