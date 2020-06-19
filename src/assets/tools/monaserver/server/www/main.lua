d58d62073993a06120f8c223b203a661da59d851
Anthony D'HIERRE <anthony.dhierre@agilitest.com>
2020-06-19 15:57:27 +0200

    Edit random string method

--- src/assets/tools/monaserver/server/www/main.lua
+++ src/assets/tools/monaserver/server/www/main.lua
@@ -36,12 +36,16 @@ function close()
     end
 end
 
-function RandomVariable(length)
-	local res = ""
-	for i = 1, length do
-		res = res .. string.char(math.random(97, 122))
-	end
-	return res
+local charset = {}  do -- [0-9a-zA-Z]
+	for c = 48, 57  do table.insert(charset, string.char(c)) end
+	for c = 65, 90  do table.insert(charset, string.char(c)) end
+	for c = 97, 122 do table.insert(charset, string.char(c)) end
+end
+
+local function randomString(length)
+	if not length or length <= 0 then return '' end
+	math.randomseed(os.clock()^5)
+	return randomString(length - 1) .. charset[math.random(1, #charset)]
 end
 
 function onConnection(client,type,...)
@@ -65,7 +69,7 @@ function onConnection(client,type,...)
 		end
 
 		if data["info"]["identifier"] == nil then
-			data["info"]["identifier"] = RandomVariable(6)
+			data["info"]["identifier"] = randomString(6)
 		end
 
 		for i=0, #devices do devices[i]=nil end
