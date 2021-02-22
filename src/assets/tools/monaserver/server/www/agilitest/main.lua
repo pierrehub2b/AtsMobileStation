7d8595dbc21cb07b65cde601431886890b26c75b
Anthony D'HIERRE <anthony.dhierre@agilitest.com>
2021-02-22 11:33:40 +0100

    remove monaserver

--- src/assets/tools/monaserver/server/www/agilitest/main.lua
+++
@@ -1,7 +0,0 @@
-function onConnection(client)
-
-	agilitestEditor = client
-
-	return { devices = devices, name = data["info"]["name"], description = data["info"]["description"], httpPort = mona.configs.HTTP.port }
-
-end
